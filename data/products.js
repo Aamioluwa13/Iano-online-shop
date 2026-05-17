a4d1ce778280078cf1d56da44f1e65f5cc21662b
Aamioluwa13 <godwinmark587@gmail.com>
2026-05-17 19:44:27 +0100

    Add brand logo

--- data/products.js
+++ data/products.js
@@ -3,70 +3,70 @@ const products = [
   {
     id: 'p1',
     name: 'Classic Iano Tee',
-    price: 29.99,
+    price: 15.99,
     description: 'A comfortable classic t-shirt.',
     image: '/images/b2.png'
   },
   {
     id: 'p2',
     name: 'Premium Iano Hoodie [White]',
-    price: 59.99,
+    price: 31.99,
     description: 'Cozy hoodie for chilly days.',
     image: '/images/b1.png'
   },
   {
     id: 'p3',
     name: 'Premium Iano Hoodie [Brown]',
-    price: 14.99,
+    price: 29.99,
     description: 'Cozy hoodie for chilly days',
     image: '/images/a6.png'
   },
   {
     id: 'p4',
     name: 'Classic Iano Tee [Gold]',
-    price: 89.99,
+    price: 15.99,
     description: 'A comfortable classic t-shirt.',
     image: '/images/a2.png'
   },
   {
     id: 'p5',
     name: 'Classic Iano Tee [Black]',
-    price: 129.99,
+    price: 15.99,
     description: 'A comfortable classic t-shirt',
     image: '/images/a1.png'
   },
   {
     id: 'p6',
     name: 'Classic Iano Tee [V-NECK] ',
-    price: 79.99,
+    price: 15.99,
     description: 'A comfortable classic t-shirt.',
     image: '/images/a9.png'
   },
   {
     id: 'p7',
     name: 'Premium Iano Hoodie [Gold]',
-    price: 79.99,
+    price: 29.99,
     description: 'Cozy hoodie for chilly days',
     image: '/images/a9.png'
   },
    {
     id: 'p8',
     name: 'Premium Iano Hoodie [Grey]',
-    price: 79.99,
+    price: 29.99,
     description: 'Cozy hoodie for chilly days',
     image: '/images/a5.png'
   },
   {
     id: 'p9',
     name: 'Premium Iano Hoodie [Blue]',
-    price: 79.99,
+    price: 29.99,
     description: 'Cozy hoodie for chilly days',
     image: '/images/a7.png'
   },
   {
     id: 'p10',
     name: 'Premium Polo Tracksuit [Blue]',
-    price: 79.99,
+    price: 24.99,
     description: 'Effortless Everyday Style',
     image: '/images/a8.png'
   }
